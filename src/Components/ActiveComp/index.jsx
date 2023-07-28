9630ed8a015b6ca916ee50748b6efe4f4221f68e
jet <88599825+jjeett0007@users.noreply.github.com>
2023-07-28 18:10:29 +0000

    hello

--- src/Components/ActiveComp/index.jsx
+++ src/Components/ActiveComp/index.jsx
@@ -1,34 +1,63 @@
 import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
 
-const ActiveTask = ({ savedInputs, setSavedInputs }) => {
-  const [ActiveTasks, setActiveTasks] = useState([]);
+const ActiveTask = ({ savedInputs, setSavedInputs, completedTasks }) => {
+  const [activeTasks, setActiveTasks] = useState([]);
 
   useEffect(() => {
-    const storedActiveTasks = JSON.parse(
-      localStorage.getItem("ActiveTask") || "[]"
-    );
+    const storedActiveTasks = JSON.parse(localStorage.getItem("ActiveTask") || "[]");
     setActiveTasks(storedActiveTasks);
-    setSavedInputs([...savedInputs]);
   }, []);
+
+  const handleCheckboxChange = (event, id) => {
+    const { checked } = event.target;
+
+    // Update the activeTasks state and remove from completedTasks when a task is checked/unchecked in ActiveTask
+    const updatedActiveTasks = activeTasks.filter((task) => task.id !== id);
+    if (!checked) {
+      const taskToAdd = savedInputs.find((input) => input.id === id);
+      if (taskToAdd) {
+        updatedActiveTasks.push(taskToAdd);
+      }
+    }
+    setActiveTasks(updatedActiveTasks);
+
+    // Update the userInput state when a task is checked/unchecked in ActiveTask
+    const updatedSavedInputs = savedInputs.map((input) =>
+      input.id === id ? { ...input, completed: checked } : input
+    );
+    setSavedInputs(updatedSavedInputs);
+    localStorage.setItem("userInputs", JSON.stringify(updatedSavedInputs));
+
+    // Update the completedTasks state and LocalStorage when a task is checked/unchecked in ActiveTask
+    const updatedCompletedTasks = completedTasks.filter((task) => task.id !== id);
+    if (checked) {
+      const taskToAdd = savedInputs.find((input) => input.id === id);
+      if (taskToAdd) {
+        updatedCompletedTasks.push(taskToAdd);
+      }
+    }
+    localStorage.setItem("completedTasks", JSON.stringify(updatedCompletedTasks));
+  };
+
   return (
     <>
-      {ActiveTasks.length > 0 ? (
+      {activeTasks.length > 0 ? (
         <>
-          {ActiveTasks.map((task) => (
-            <Wrapper>
-                <input
+          {activeTasks.map((task) => (
+            <Wrapper key={task.id}>
+              <input
                 type="checkbox"
                 id={task.id}
-                // onChange={(e) => handleCheckboxChange(e, input.id)}
-                checked={task.completed || false} // Provide a default value for completed
+                onChange={(e) => handleCheckboxChange(e, task.id)}
+                checked={completedTasks.some((completedTask) => completedTask.id === task.id)}
               />
-              <p key={task.id}>{task.text}</p>
+              <p>{task.text}</p>
             </Wrapper>
           ))}
         </>
       ) : (
-        <p>No completed tasks found in local storage.</p>
+        <p>No active tasks found in local storage.</p>
       )}
     </>
   );
